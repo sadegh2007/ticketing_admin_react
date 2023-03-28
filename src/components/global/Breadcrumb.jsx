20a4d5b2e6b8f28e759551d6a917db385517bd1e
sadeqhajizadeh <sadegh.h.2007@gmail.com>
2023-03-28 19:41:35 +0330

    fix breadcrumb dashboard route

--- src/components/global/Breadcrumb.jsx
+++ src/components/global/Breadcrumb.jsx
@@ -2,21 +2,18 @@ import {Link} from "react-router-dom";
 import {ReactSVG} from "react-svg";
 import {GetTenant} from "../../services/AuthService.js";
 
-const Breadcrumb = (props) => {
-
-    return(
-        <div className="text-sm breadcrumbs">
-            <ul>
-                <li><Link to={`/${GetTenant()}/admin/dashboard`} className="flex items-center">
-                    <ReactSVG className="menu-svg" src="/src/assets/svgs/layout-dashboard.svg"/>
-                    <span className="mr-1">داشبورد</span>
-                </Link></li>
-                {
-                    props.items && props.items.length > 0 ? props.items.map((item, index) => <li key={index}>{item.to === '#' ? <span className="font-medium">{item.title}</span> : <Link to={item.to}>{item.title}</Link>}</li>) : ''
-                }
-            </ul>
-        </div>
-    );
-}
+const Breadcrumb = ({items}) => (
+    <div className="text-sm breadcrumbs">
+        <ul>
+            <li><Link to={`/${GetTenant()}/admin/dashboard`} className="flex items-center">
+                <ReactSVG className="menu-svg" src="/src/assets/svgs/layout-dashboard.svg"/>
+                <span className="mr-1">داشبورد</span>
+            </Link></li>
+            {
+                items && items.length > 0 ? items.map((item, index) => <li key={index}>{item.to === '#' ? <span className="font-medium">{item.title}</span> : <Link to={item.to}>{item.title}</Link>}</li>) : ''
+            }
+        </ul>
+    </div>
+)
 
 export default Breadcrumb;
