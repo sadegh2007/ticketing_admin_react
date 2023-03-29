a243ff8b0b916397a1e0cccb420615521e145577
sadeqhajizadeh <sadegh.h.2007@gmail.com>
2023-03-29 19:25:09 +0330

    add users to department crud

--- src/modals/Deparments/DepartmentModal.jsx
+++ src/modals/Deparments/DepartmentModal.jsx
@@ -3,24 +3,51 @@ import {useForm} from "react-hook-form";
 import {useContext, useEffect, useState} from "react";
 import {appContext} from "../../context/AppContext.js";
 import {handleError} from "../../services/GlobalService.js";
-import {CreateDepartment, UpdateDepartment} from "../../services/DepartmentApiService.js";
+import {CreateDepartment, GetDepartmentById, UpdateDepartment} from "../../services/DepartmentApiService.js";
+import ServerSideSelect from "../../components/SelectBox/ServerSideSelect.jsx";
+import ApiConstants from "../../general/ApiConstants.js";
 
 const DepartmentModal = ({department, show, closeModal}) => {
     const [title, setTitle] = useState('');
+    const [users, setUsers] = useState();
     const { register, formState: { errors } } = useForm();
     const {showMainLoader, toggleMainLoader} = useContext(appContext);
 
     useEffect(() => {
         if (department) {
             setTitle(department.title);
+            toggleMainLoader(true);
+
+            GetDepartmentById(department.id)
+                .then(res => {
+                    if (res.departmentUsers && res.departmentUsers.length > 0) {
+                        setUsers(res.departmentUsers.map(x => {
+                            return {
+                                label: x.user.fullName,
+                                value: x.user.id
+                            }
+                        }));
+                    }
+
+                    toggleMainLoader(false);
+                })
+                .catch(err => {
+                    toggleMainLoader(false);
+                    handleError(err.response);
+                });
         }
     }, [department]);
 
     const saveDepartment = () => {
         toggleMainLoader(true);
 
+        const data = {
+            title: title,
+            userIds: users.map(x => x.value)
+        }
+
         if (department) {
-            UpdateDepartment(department.id, {title})
+            UpdateDepartment(department.id, data)
                 .then(res => {
                     toggleMainLoader(false);
                     closeModal(true);
@@ -31,7 +58,7 @@ const DepartmentModal = ({department, show, closeModal}) => {
                 });
         }
         else {
-            CreateDepartment({title})
+            CreateDepartment(data)
                 .then(res => {
                     toggleMainLoader(false);
                     closeModal(true);
@@ -41,6 +68,15 @@ const DepartmentModal = ({department, show, closeModal}) => {
                     handleError(err.response);
                 });
         }
+    }
+
+    const formatUserSelect = (data) => {
+        return data.items?.map((x) => {
+            return {
+                label: x.fullName,
+                value: x.id,
+            };
+        }) ?? [];
     }
 
     return (
@@ -71,10 +107,25 @@ const DepartmentModal = ({department, show, closeModal}) => {
                             className={`input input-bordered rounded focus:outline-none text-sm w-full placeholder-gray-600 ${errors.title && ''}`}
                         />
                     </div>
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">کاربران</span>
+                        </label>
+                        <ServerSideSelect
+                            placeholder="انتخاب کاربران..."
+                            url={ApiConstants.Users.List}
+                            method={'POST'}
+                            onSelect={setUsers}
+                            value={users}
+                            multiple={true}
+                            formatData={formatUserSelect}
+                        />
+                    </div>
                 </div>
 
                 <div className="modal-action">
-                    <button disabled={!title || title.length === 0} onClick={saveDepartment} className="btn rounded btn-success text-white">ثبت</button>
+                    <button disabled={!title || title.length === 0 || !users || users.length === 0} onClick={saveDepartment} className="btn rounded btn-success text-white">ثبت</button>
                 </div>
             </div>
         </div>
